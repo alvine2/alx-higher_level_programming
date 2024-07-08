26a77302aacd639d1da5d7f216dbb75d80484044
alvine2 <alvinekinyera@gmail.com>
2024-07-08 05:54:09 -0700

    upd

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials.
+"""
+Uses the GitHub API to display a GitHub ID based on given credentials.
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
-  """
-  import sys
-  import requests
-  from requests.auth import HTTPBasicAuth
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./10-my_github.py <alvine2> <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>")
+        sys.exit(1)
+    
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    response = requests.get("https://api.github.com/user", auth=auth)
+    
+    if response.status_code == 200:
+        print(response.json().get("id"))
+    else:
+        print("None")
 
-  if __name__ == "__main__":
-          auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-              r = requests.get("https://api.github.com/user", auth=auth)
-                  print(r.json().get("id"))
